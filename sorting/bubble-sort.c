4161905d85dc745bce0394c61816c6e327aaa54f
Ankit R Gadiya <git@argp.in>
2017-11-07 13:24:03 +0530

    Bubble Sort

---
+++ sorting/bubble-sort.c
@@ -0,0 +1,42 @@
+/* Bubble Sort Implementation
+ * Copyright 2017, Ankit R Gadiya
+ * MIT License
+ */
+#include <stdio.h>
+
+#define NUM 10
+
+int sample[NUM] = {3, 7, 9, 5, 1, 0, 6, 2, 4, 8};
+
+void bubbleSort(int array[], int range)
+{
+	int change, i;
+	do {
+		change = 0;
+		for (i = 0; i < range - 1; i++) {
+			if (array[i] > array[i + 1]) {
+				int temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				change++;
+			}
+		}
+	} while (change != 0);
+	return;
+}
+
+int main (int argc, char *argv[])
+{
+	for (int i = 0; i < NUM; i++)
+		printf("%d ", sample[i]);
+
+	printf("\n");
+
+	bubbleSort(sample, NUM);
+
+	for (int i = 0; i < NUM; i++)
+		printf("%d ", sample[i]);
+
+	printf("\n");
+	return 0;
+}
